1f393c6408839a576edcccb7630c29b234c2671c
Alvin Kita <Alvin.Kita@outlook.fr>
2024-03-21 16:08:39 +0100

    Snap Base

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {FaceSnap} from "./models/face-snap.model";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  myFirstSnap!: FaceSnap;
+  mySchoolSnap! : FaceSnap;
+  myWorkSnap! : FaceSnap;
 
+  ngOnInit() {
+    this.myFirstSnap = new FaceSnap(
+      'Mouss',
+      'Mon petit chat espiègle',
+      new Date(),
+      3,
+      '../../assets/images/mouss.png'
+    );
+
+    this.mySchoolSnap = new FaceSnap(
+      'L\'IPI',
+      'L\'école ou je passe mon diplome',
+      new Date(),
+      10,
+      'https://www.ipi-ecoles.com/wp-content/uploads/2020/09/ipi-lyon-hep-campus.jpg'
+    );
+
+    this.myWorkSnap = new FaceSnap(
+      'Paredes Orapi',
+      'Là où je travail en alternance de dévellopeur',
+      new Date(),
+      152,
+      'https://th.bing.com/th?id=OIF.OURne%2fLBGR1TLzLjoRBhHQ&w=316&h=180&c=7&r=0&o=5&pid=1.7'
+    )
+  }
 }
+
--- src/app/face-snap/face-snap.component.ts
+++ src/app/face-snap/face-snap.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import {FaceSnap} from "../models/face-snap.model";
 
 @Component({
   selector: 'app-face-snap',
   templateUrl: './face-snap.component.html',
   styleUrl: './face-snap.component.scss'
 })
-export class FaceSnapComponent {
+export class FaceSnapComponent implements OnInit {
+  @Input() faceSnap!: FaceSnap;
+
+  title!: string;
+  description!: string;
+  createdDate!: Date;
+  snaps!: number;
+  imageUrl!: string;
+  buttonText!: string;
+
+  ngOnInit() {
+    this.buttonText = "Oh Snap!";
+  }
+
+   /////////////
+  // METHODS //
+ /////////////
+
+  onSnap() {
+    if (this.buttonText == "Oh Snap!") {
+      this.faceSnap.snaps++;
+      this.buttonText = "Oups Snap!";
+    } else if (this.buttonText == "Oups Snap!"){
+      this.faceSnap.snaps--;
+      this.buttonText = "Oh Snap!";
+    }
+  }
 
 }
